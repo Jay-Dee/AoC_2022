223805aa991f3bd88922492441e74607c456cda6
Jay-Dee <johnsondesouza@gmail.com>
2022-12-13 09:27:43 +0000

    Day2 changes

---
+++ Day2/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AoC_2022.Utils;
+using Day2;
+
+int finalScore = 0;
+int expectedScore = 0;
+var computedScores = new List<Score>();
+var expectedScores = new List<Score>();
+Console.WriteLine("AoC 2022 : Day 2");
+
+
+var scores = FileUtils.GetContent("Scores.txt");
+
+foreach (string score in scores)
+{
+    var computedScore = new Score(score);
+
+    computedScores.Add(computedScore);
+}
+
+Console.WriteLine(computedScores.Sum(s => s.RoundScore));
+
+foreach (string score in scores)
+{
+    var requiredPlay = Score.GetRequiredPlay(score);
+
+    var requiredScore = new Score(requiredPlay);
+    expectedScores.Add(requiredScore);
+}
+
+Console.WriteLine(expectedScores.Sum(s => s.RoundScore));
---
+++ Day2/Score.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2 {
+    internal class Score {
+
+        private readonly Dictionary<string, int> _scoreMap = new()
+        {
+            { "A X", 4},
+            { "A Y", 8},
+            { "A Z", 3},
+            { "B X", 1},
+            { "B Y", 5},
+            { "B Z", 9},
+            { "C X", 7},
+            { "C Y", 2},
+            { "C Z", 6}
+        };
+
+        private static readonly Dictionary<string, string> __resultMap = new()
+        {//x - loose, y - draw, z - win; a = rock, b = paper, c = scissors
+            {"A X", "Z"},
+            {"A Y", "X"},
+            {"A Z", "Y"},
+            {"B X", "X"},
+            {"B Y", "Y"},
+            {"B Z", "Z"},
+            {"C X", "Y"},
+            {"C Y", "Z"},
+            {"C Z", "X"},
+        };
+
+        public int RoundScore { get; }
+        
+        public Score(string score)
+        {
+            RoundScore = _scoreMap[score];
+        }
+
+        public static string GetRequiredPlay(string score)
+        {
+            var requiredPlay = __resultMap[score];
+
+            return $"{score.Split(" ")[0]} {requiredPlay}";
+        }
+    }
+}
