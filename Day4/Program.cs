a406d9e778e9bfada93f2371c692d2f3b31184e1
Jay-Dee <johnsondesouza@gmail.com>
2022-12-15 11:28:28 +0000

    Day4 implementation

---
+++ Day4/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using AoC_2022.Utils;
+
+Console.WriteLine("Day4!");
+int fullyContains = 0;
+int overlaps = 0;
+var sections = FileUtils.GetContent("Sections.txt");
+
+foreach (string section in sections)
+{
+    var assignments = section.Split(",").Select(s => s.Split("-")).ToList();
+
+    Console.WriteLine($"Left = {string.Join(",", assignments[0])}");
+    Console.WriteLine($"Right = {string.Join(",", assignments[1])}");
+
+    //if (int.Parse(assignments[0][0]) <= int.Parse(assignments[1][0]) 
+    //    && int.Parse(assignments[0][1]) >= int.Parse(assignments[1][1]))
+    //{
+    //    Console.WriteLine($"Left = {string.Join(",", assignments[0])} contains Right = {string.Join(",", assignments[1])}");
+    //    fullyContains++;
+    //    continue;
+    //}
+
+    //if (int.Parse(assignments[1][0]) <= int.Parse(assignments[0][0])
+    //    && int.Parse(assignments[1][1]) >= int.Parse(assignments[0][1])) {
+    //    Console.WriteLine($"Right = {string.Join(",", assignments[1])} contains Left = {string.Join(",", assignments[0])}");
+    //    fullyContains++;
+    //    continue;
+    //}
+
+    if ((int.Parse(assignments[0][0]) <= int.Parse(assignments[1][0]) &&
+        int.Parse(assignments[1][0]) <= int.Parse(assignments[0][1]))
+        ||
+        (int.Parse(assignments[0][0]) <= int.Parse(assignments[1][1]) &&
+         int.Parse(assignments[1][1]) <= int.Parse(assignments[0][1])))
+    {
+        Console.WriteLine($"Left = {string.Join(",", assignments[0])} overlaps Right = {string.Join(",", assignments[1])}");
+        overlaps++;
+        continue;
+    }
+
+    if ((int.Parse(assignments[1][0]) <= int.Parse(assignments[0][0]) &&
+         int.Parse(assignments[0][0]) <= int.Parse(assignments[1][1]))
+        ||
+        (int.Parse(assignments[1][0]) <= int.Parse(assignments[0][1]) &&
+         int.Parse(assignments[0][1]) <= int.Parse(assignments[1][1]))) {
+        Console.WriteLine($"Left = {string.Join(",", assignments[0])} overlaps Right = {string.Join(",", assignments[1])}");
+        overlaps++;
+    }
+}
+
+Console.WriteLine(overlaps);
+
