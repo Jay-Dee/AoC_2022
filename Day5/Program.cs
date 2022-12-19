4fb1d7db89909ef2d4ed279d8706140609b46f81
Jay-Dee <johnsondesouza@gmail.com>
2022-12-19 11:02:24 +0000

    Combined part 1 and part 2 for Day5

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -3,7 +3,8 @@
 using AoC_2022.Utils;
 
 Console.WriteLine("Day5");
-var crates = new Dictionary<int, Stack<string>>();
+var singleMoveCrates = new Dictionary<int, Stack<string>>();
+var multiMoveCrates = new Dictionary<int, Stack<string>>();
 var initialLayout = FileUtils.GetContent("InitialLayout.txt").Reverse().ToList();
 var crateMoves = FileUtils.GetContent("CrateMoves.txt");
 
@@ -11,12 +12,13 @@ var crateConfiguration = initialLayout.First();
 var numberOfCrates = crateConfiguration.Split(" ").Count(s => !string.IsNullOrWhiteSpace(s));
 for (int i = 1; i <= numberOfCrates; i++)
 {
-    crates[i] = new Stack<string>();
+    singleMoveCrates[i] = new Stack<string>();
+    multiMoveCrates[i] = new Stack<string>();
 }
 
 foreach (string layout in initialLayout.Skip(1))
 {
-    foreach (var crate in crates)
+    foreach (var crate in multiMoveCrates)
     {
         int startIndex = 0;
         if (crate.Key > 1)
@@ -28,6 +30,7 @@ foreach (string layout in initialLayout.Skip(1))
         if (!string.IsNullOrWhiteSpace(crateValue))
         {
             crate.Value.Push(crateValue);
+            singleMoveCrates[crate.Key].Push(crateValue);
         }
     }
 }
@@ -41,19 +44,21 @@ foreach (string move in crateMoves)
     var toCrate = int.Parse(moveConfiguration[5]);
 
     Console.WriteLine($"Move {itemsToMove} ");
-    Console.WriteLine($"From {fromCrate} : {string.Join(",", crates[fromCrate])}");
-    Console.WriteLine($"To {toCrate} : {string.Join(",", crates[toCrate])}");
+    Console.WriteLine($"From {fromCrate} : {string.Join(",", multiMoveCrates[fromCrate])}");
+    Console.WriteLine($"To {toCrate} : {string.Join(",", multiMoveCrates[toCrate])}");
     for (int moveItemCounter = 0; moveItemCounter < itemsToMove; moveItemCounter++)
     {
-        tempCrate.Push(crates[fromCrate].Pop());
+        singleMoveCrates[toCrate].Push(singleMoveCrates[fromCrate].Pop());
+        tempCrate.Push(multiMoveCrates[fromCrate].Pop());
     }
     for (int moveItemCounter = 0; moveItemCounter < itemsToMove; moveItemCounter++) 
     {
-        crates[toCrate].Push(tempCrate.Pop());
+        multiMoveCrates[toCrate].Push(tempCrate.Pop());
     }
 
-    Console.WriteLine($"After move {string.Join(",", crates[toCrate])}");
+    Console.WriteLine($"After move {string.Join(",", multiMoveCrates[toCrate])}");
 }
 
-Console.WriteLine($"{string.Join("", crates.Values.Select(v => v.Pop()))}");
+Console.WriteLine($"{string.Join("", singleMoveCrates.Values.Select(v => v.Pop()))}");
+Console.WriteLine($"{string.Join("", multiMoveCrates.Values.Select(v => v.Pop()))}");
 
